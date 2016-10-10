f52c0caf9fa68ca5380524b76494118486415ab5
John Nico <jnico810@gmail.com>
2016-10-10 16:53:23 -0400

    Complete gradient on show page

--- frontend/components/tracks/track_index_item.jsx
+++ frontend/components/tracks/track_index_item.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const TrackIndexItem = ({track, url}) => (
+<Link to={url}>
+  <li className="track-list-item">
+    <div className="track-list-item-num">
+      <span>num</span>
+    </div>
 
-<li className="track-list-item">
-  <Link to={url}>
-    <div className="track-list-item-album-art">
-      <img src={ window.prodigyAssets.defaultImage }></img>
-    </div>
-    <div className="track-list-item-content">
-      <h2>{ track.title }</h2>
-      <h4>{ track.artist }</h4>
-      <p>{ track.description}</p>
-    </div>
-  </Link>
-</li>
+      <div className="track-list-item-album-art">
+        <img src={ window.prodigyAssets.defaultImage }></img>
+      </div>
+      <div className="track-list-item-content">
+        <h2>{ track.title }</h2>
+        <h4>{ track.artist }</h4>
+        <p>{ track.description}</p>
+      </div>
+  </li>
+</Link>
 );
 
 
