cbd931678ae7a9e3f415daaab3fb02a905f9879e
John Nico <jnico810@gmail.com>
2016-10-24 16:51:55 -0400

    Add https protocol to paperclip

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,8 @@ module Prodigy
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
         :s3_region => ENV["s3_region"]
-      }
+      },
+      :s3_protocol => :https
     }
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
