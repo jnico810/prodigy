bd0329e62a9dcc25a9d89d9a8752393a5bc343e5
John Nico <jnico810@gmail.com>
2016-10-28 13:13:30 -0400

    Update youtube query

--- frontend/util/youtube_api_util.js
+++ frontend/util/youtube_api_util.js
@@ -2,7 +2,7 @@ export const requestYoutubeUrl = (querry, success, error) => {
   $.ajax({
     method: "GET",
     // url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=${querry}&safeSearch=strict&type=video&fields=items%2Fid%2FvideoId&key=AIzaSyAWHNIuU3OX8s_zphqzrFmJsYvstRkAuxM`,
-    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&videoEmbeddable=true&q=${querry}&safeSearch=moderate&type=video&key=AIzaSyAWHNIuU3OX8s_zphqzrFmJsYvstRkAuxM`,
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&videoEmbeddable=true&q=${querry}+Lyrics+-Remix+-remix+-REMIX+-live+-LIVE+-Live&safeSearch=moderate&type=video&key=AIzaSyAWHNIuU3OX8s_zphqzrFmJsYvstRkAuxM`,
     success,
     error
   });
