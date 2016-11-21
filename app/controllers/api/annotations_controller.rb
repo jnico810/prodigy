058b3b68b0767d6533cb28090f6b7737cc2f7fc7
JOHN NICO <jnico810@gmail.com>
2016-11-21 17:52:48 -0500

    cleanup

--- app/controllers/api/annotations_controller.rb
+++ app/controllers/api/annotations_controller.rb
@@ -1,16 +1,6 @@
 class Api::AnnotationsController < ApplicationController
 
-  #  id         :integer          not null, primary key
-  #  author_id  :integer          not null
-  #  track_id   :integer          not null
-  #  start_idx  :integer          not null
-  #  end_idx    :integer          not null
-  #  created_at :datetime         not null
-  #  updated_at :datetime         not null
-  #
-
   def create
-
     @annotation = Annotation.new(author_id: current_user.id,
     start_idx: annotation_params[:start_idx],
     end_idx: annotation_params[:end_idx],
